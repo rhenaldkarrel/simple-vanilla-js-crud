57e436ee39053243acd303839388ff2d13558f7d
Rhenald <renaldkarel@gmail.com>
2023-05-27 14:04:27 +0700

    feat: add default user admin

--- js/core/db.js
+++ js/core/db.js
@@ -1,4 +1,12 @@
 import { STORAGE_KEY } from './constants.js';
 import { getLocalStorage } from './utils.js';
 
-export const users = getLocalStorage(STORAGE_KEY) ?? [];
+export const users = getLocalStorage(STORAGE_KEY) ?? [
+	{
+		fullname: 'Admin',
+		email: 'admin@gmail.com',
+		password: '123',
+		age: 0,
+		company: '-',
+	},
+];
